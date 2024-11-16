1d6895265fa68b09fdc4a4b6bcff87c15093e377
Minjun Guan <minjun.guan@tetrapak.com>
2024-11-16 18:38:41 +0800

    update cloudfront url.

--- src/main/java/com/nus/edu/se/notification/webSocket/WebSocketConfig.java
+++ src/main/java/com/nus/edu/se/notification/webSocket/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://d20bde94kgtbik.cloudfront.net")
+                .setAllowedOrigins("https://d1tdd75mfyl36s.cloudfront.net")
                 .withSockJS();
     }
 }
